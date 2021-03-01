b52b46a3b2ecd5fd7b6bf1e2ad2ab5bfb41e6883
Ankitapatil <ankitakhairnar10@gmail.com>
2021-03-01 09:03:55 +0530

    Added: Array problem

---
+++ Array/primeFactors.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+
+read -p "enter the number : " n
+
+d=$(echo $n | awk '{print sqrt($n)}' | awk -F. '{print $1}')
+
+count=0 #counter
+flag=0
+even=0
+# to eleminate all even
+for((; $(($n%2)) == 0;))
+do
+   n=$(($n/2))
+   A[count]=2
+   even=1
+done
+
+# to eleminate all odd
+for ((i=3; i<=$d; i=$(($i+2)) ))
+do
+   for((; $(($n%$i))==0;))
+   do
+      n=$(($n/$i))
+      #echo $i
+      if [ $even -eq 0 ]
+      then
+         A[count]=$i
+         even=1;
+         ((count++))
+      fi
+      if [ ${A[count-1]} != $i ]
+      then
+         A[((++count))]=$i
+      fi
+   done
+done
+   if [ $n -gt 2 ]
+  then
+          A[++count]-$n
+   fi
+     echo ${A[@]}
