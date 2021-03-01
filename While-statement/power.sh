a876e727e3680157454e7cee011efd073fa6cc51
Ankitapatil <ankitakhairnar10@gmail.com>
2021-03-01 15:58:13 +0530

    Added: power of two

---
+++ While-statement/power.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+read -p "Enter a whole number: " x;
+
+counter=0
+while (( counter<=x ))
+do
+
+ if (( counter<9 ))
+  then
+         var=$((2**$counter))
+         echo $counter=$var
+
+    ((counter++))
+    else
+           echo above 256 are not allowed
+fi
+done
+
